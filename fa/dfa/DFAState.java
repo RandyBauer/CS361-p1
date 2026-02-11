926ad3a4f7de8337c4f9e38dfc3047a365382403
Randy <randybauer@u.boisestate.edu>
2026-02-11 16:00:51 -0700

    Implemented some DFA class methods

--- fa/dfa/DFAState.java
+++ fa/dfa/DFAState.java
@@ -1,5 +1,19 @@
 package fa.dfa;
 
-public class DFAState {
+/**
+ * JavaDoc Description TODO
+ * @author Randy Bauer
+ * @author Oliver Hill
+ */
+public class DFAState extends fa.State{
+    
+    /**
+     * Default constructor
+     * @param name String label for the state
+     */
+    public DFAState(String name) {
+        super(name);
+    }
+
     
 }
